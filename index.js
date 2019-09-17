ebad3bf111f17cf4d4ffe4b8b73e9c42856b98eb
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-17 16:26:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ function createPost(){
   postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
 }
 
-// function postComment() {
-//   var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
 
-//   var commentText = document.getElementById("commentText").value;
-//   var commenterName = document.getElementById("commenter").value;
+  var commentText = document.getElementById("commentText").value;
+  var commenterName = document.getElementById("commenter").value;
 
-//   var commentsSection = document.getElementById("comments");
-//   commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
+  var commentsSection = document.getElementById("comments");
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
 }
