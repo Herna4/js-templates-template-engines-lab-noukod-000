929fbcfdcc0cbe702d0d94bc553e13819b9785f0
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-17 16:11:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function createPost(){
   
   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
   
-// document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+  var blogSection = postTemplate()
 
 //   var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
 //   var commentsSection = commentsTemplate();
